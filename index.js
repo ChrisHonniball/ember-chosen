5398b1d09a4e7558944049c1c44c57746d86265b
Chris Honniball <chonniball@gozooga.com>
2015-06-26 10:35:54 -0400

    Dummy app updates

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
 /* jshint node: true */
 'use strict';
 
-var pickFiles = require('broccoli-funnel');
-var merge = require('lodash.merge');
+var funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-chosen',
   included: function(app) {
     this._super.included(app);
 
-    // Setup default options for ember-chosen
-    var options = merge({
-      'jQuery': true,
-      'importChosenCSS': true
-    }, app.options['ember-chosen'] || {});
-
-    options.chosenJSType = options.jQuery ? 'jquery' : 'proto';
-
-    // Update `ember-chosen` options on our `app` with updated hash
-    app.options['ember-chosen'] = options;
-
     // Import the correct JS for chosen
-    app.import(app.bowerDirectory + '/chosen/chosen.' + options.chosenJSType + '.min.js');
+    app.import(app.bowerDirectory + '/chosen/chosen.jquery.min.js');
 
     // Import Chosen CSS (done by default)
-    if(options.importChosenCSS) { app.import(app.bowerDirectory + '/chosen/chosen.min.css'); }
+    app.import(app.bowerDirectory + '/chosen/chosen.min.css');
   },
   treeForPublic: function(treeName) {
     var tree;
 
-    // Only include the Chosen sprites if we're including Chosen CSS in the build
-    if(this.app.options['ember-chosen'].importChosenCSS) {
-      tree = pickFiles(this.app.bowerDirectory + '/chosen', {
-        srcDir: '/',
-        files: ['*.png'],
-        destDir: '/assets'
-      });
-    }
+    tree = funnel(this.app.bowerDirectory + '/chosen', {
+      include: ['*.png'],
+      destDir: '/assets'
+    });
 
     return tree;
   }
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,7 +6,6 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('options');
 });
 
 export default Router;
